03742c70f02a5adf70c4c65417af87454cd2cf53
Aksaraphak Oungsawad <gidgdofudvud@gmail.com>
2026-09-13 12:09:53 +0700

    add all final project folders

---
+++ JS 116710905105-8/js/script.js
@@ -0,0 +1,5 @@
+console.log("Hello Javascript");
+console.log("Hello Computer Science");
+document.getElementById("text").innerHTML = "This is first time for javascript";
+document.writeln("Hello document write");
+window.alert("welcome to my page");
